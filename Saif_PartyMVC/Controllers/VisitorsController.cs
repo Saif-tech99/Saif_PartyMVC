3cb133f4c56c65a78f5362a250f9b93becfc025f
Saif Al-adilee <saifa9697@gmail.com>
2021-06-07 14:28:40 +0200

    Add project files.

---
+++ Saif_PartyMVC/Controllers/VisitorsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Saif_PartyMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Saif_PartyMVC.Controllers
+{
+    public class VisitorsController : Controller
+    {
+
+        private List<Visitor> _visitors = new List<Visitor>();
+
+        // GET - Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST - Create
+        [HttpPost]
+        public IActionResult Create(Visitor visitor)
+        {
+            _visitors.Add(visitor);
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            TempData["TotalGuests"] = _visitors.Count;
+            TempData["TotalFamilyMembers"] = _visitors.Count(x => x.IsFamily == true);
+            TempData["YoungestGuestAge"] = _visitors.Count(x => x.Age < x.Age);
+            TempData["OldestguestAge"] = _visitors.Count(x => x.Age > x.Age);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Summary(int id)
+        {
+            int totalG = (int)TempData.Peek("TotalGuests");
+            int totalF = (int)TempData.Peek("TotalFamilyMembers");
+            int totalY = (int)TempData.Peek("YoungestGuestAge");
+            int totalO = (int)TempData.Peek("OldestguestAge");
+
+            totalG = _visitors.Count;
+            totalF = _visitors.Count(x => x.IsFamily == true);
+            totalY = _visitors.Count(x => x.Age < x.Age);
+            totalO = _visitors.Count(x => x.Age > x.Age);
+
+            TempData["TotalGuests"] = totalG;
+            TempData["TotalFamilyMembers"] = totalF;
+            TempData["YoungestGuestAge"] = totalY;
+            TempData["OldestguestAge"] = totalO;
+
+            return View();
+        }
+
+    }
+}
---
+++ Saif_PartyMVC/Models/Visitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Saif_PartyMVC.Models
+{
+    public class Visitor
+    {
+        [Required(ErrorMessage = "Please enter your First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Second Name")]
+        public string SecondName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email Address")]
+        [EmailAddress(ErrorMessage = "Email format is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Tel Number")]
+        public string Tel { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Age")]
+        [Range(18, 60, ErrorMessage = "Age must be between  18 and 60")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Address")]
+        public string Addtess { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Postal Code")]
+        [Range(1000, 1500, ErrorMessage = "Postal code is not valid")]
+        public int PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your City")]
+        public string City { get; set; }
+
+        public bool IsFamily { get; set; }
+    }
+}
